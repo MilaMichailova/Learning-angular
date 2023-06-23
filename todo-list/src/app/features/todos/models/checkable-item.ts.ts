96850fcd944810da3a42f3ee41e7c3dee09e7425
MilaMichailova <kosarevitch.mila2010@gmail.com>
2023-06-23 10:42:14 +0300

    Все модели снабдила комментариями (синтаксис JSDoc)

--- todo-list/src/app/features/todos/models/checkable-item.ts.ts
+++ todo-list/src/app/features/todos/models/checkable-item.ts.ts
@@ -1,3 +1,10 @@
+/**
+ * создает интерфейс CheckableItem как кастомный тип днных без реализации .
+ *
+ * @property  {boolean} checked указывает выбрана ли задача.
+ * @property  {any} data данные .
+ */
+
 export interface CheckableItem {
   checked: boolean;
   data: any;
--- todo-list/src/app/features/todos/models/todo-item.ts.ts
+++ todo-list/src/app/features/todos/models/todo-item.ts.ts
@@ -1,5 +1,13 @@
 import { Moment } from 'moment';
 
+/**
+ * создает интерфейс TodoItem как кастомный тип днных без реализации .
+ *
+ * @property  {string} id айди айтема.
+ * @property  {string} description описание задачи.
+ * @property  {Moment} createAt, время создания задачи.
+ */
+
 export interface TodoItem {
   id: string;
   description: string;
