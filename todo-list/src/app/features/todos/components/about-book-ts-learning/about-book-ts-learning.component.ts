ec20723dbedca6fe9fe9f7c57a7f3e158c547d6c
MilaMichailova <kosarevitch.mila2010@gmail.com>
2023-08-22 12:49:36 +0300

    Сгенерировала новое angular приложение http-client-lessonповторила пример из статьи.

---
+++ todo-list/src/app/features/todos/components/about-book-ts-learning/about-book-ts-learning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-book-ts-learning',
+  templateUrl: './about-book-ts-learning.component.html',
+  styleUrls: ['./about-book-ts-learning.component.scss']
+})
+export class AboutBookTsLearningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
